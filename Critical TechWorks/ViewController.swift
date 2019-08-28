41797cae51baad8fb85c0548449ce0ee3dbcf160
Bruno Silva <bruno.silva@bestseller.com>
2019-08-28 15:34:57 +0100

    Initial commit

---
+++ Critical TechWorks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Critical TechWorks
+//
+//  Created by Bruno Silva on 28/08/2019.
+//  Copyright © 2019 Bruno Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
